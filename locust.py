837a37d08b7b8dd9d205150ff6f354832c3b3a2b
RisHUB63 <91869180+RisHUB63@users.noreply.github.com>
2025-05-20 18:57:58 +0530

    [ADD]: Code to automate the load testing.

---
+++ locust.py
@@ -0,0 +1,15 @@
+# **3. Locust Load Tester (locustfile.py)**
+
+# ```python
+from locust import HttpUser, task, between
+
+class MyUser(HttpUser):
+    wait_time = between(1, 3)
+    host = "http://localhost:8000"
+    @task
+    def get_data(self):
+        self.client.get("/api/create")
+        self.client.get("/api/update")
+        self.client.get("/api/delete")
+        self.client.get("/api/list")
+        self.client.get("/api/format")
